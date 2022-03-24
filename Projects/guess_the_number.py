c5b9513741e97bd8352ec66d700fe1106077ce69
Adrian Drewniak <adriandrewniak19@gmail.com>
2022-03-24 16:42:22 +0100

    added user_input validator

--- Projects/guess_the_number.py
+++ Projects/guess_the_number.py
@@ -1,1 +1,19 @@
 print('Guess the number.\nChoose one from 1 - 100')
+
+
+def check_input():
+    """
+    Check if number provided by user is correct type and if is in within the range
+    :return checked number:
+    """
+    while True:
+        try:
+            user_guess = int(input())
+        except (TypeError, ValueError):
+            print('It is not a number')
+            continue
+        return user_guess
+
+
+a = check_input()
+print(a)
