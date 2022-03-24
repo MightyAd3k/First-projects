a2b5471bed42c0df2528744b57875abfecfe9e6d
Adrian Drewniak <adriandrewniak19@gmail.com>
2022-03-24 17:18:11 +0100

    added main function

--- Projects/guess_the_number.py
+++ Projects/guess_the_number.py
@@ -1,9 +1,13 @@
+from random import randint
+
 print('Guess the number.\nChoose one from 1 - 100')
 
 
 def check_input():
     """
-    Check if number provided by user is correct type and if is in within the range
+    Check if number provided by user is correct type and if is in within the range.
+
+    :rtype : int
     :return checked number:
     """
     while True:
@@ -12,8 +16,31 @@ def check_input():
         except (TypeError, ValueError):
             print('It is not a number')
             continue
+
+        if user_guess < 1 or user_guess > 100:
+            print('Number not in requested range')
+            continue
+
         return user_guess
 
 
-a = check_input()
-print(a)
+def game():
+    """Main function of the game"""
+
+    secret_number = randint(1, 100)
+    user_number = check_input()
+    counter = 0
+
+    while user_number != secret_number:
+        counter += 1
+        if user_number < secret_number:
+            print('Your number is too small')
+
+        elif user_number > secret_number:
+            print('Your number is too big')
+        user_number = check_input()
+    print(f'You win! You did it in {counter} times.')
+
+
+if __name__ == '__main__':
+    game()
